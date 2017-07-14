8a358180b2d0d018c50654b62f98b0b708e72483
studiophi <studiophi@users.noreply.github.com>
2017-07-13 22:39:42 -0400

    Made server and client keep track of who the last player (useragent) was.

--- public/javascripts/Gameplay.js
+++ public/javascripts/Gameplay.js
@@ -6,7 +6,14 @@ socket.on('receiveBoard', function (board) {
 socket.on('news', function (data) {
   console.log(data);
 });
+var amLast = false;
 socket.on('receivePlay', function (play) {
+  if (!amLast) {
+    $('.gameboard thead tr').show();
+  }
+  else {
+    amLast = false;
+  }
   var selector = ".gameboard tbody tr:nth-child(" + (6 - play.row) + ") td:nth-child(" + (play.column + 1) + ") img"
   var piece = $(selector);
 
@@ -23,11 +30,20 @@ socket.on('receivePlay', function (play) {
 });
 socket.on('receiveWin', function (win) {
   alert(win.agent + " wins!");
-  location.reload();  
+  location.reload();
+});
+
+socket.on('enableDrop', function () {
+  $('.gameboard thead tr').show();
+});
+socket.on('disableDrop', function () {
+  $('.gameboard thead tr').hide();
 });
 
 function drop(col) {
   socket.emit('play', {'column': col});
+  $('.gameboard thead tr').hide();
+  amLast = true;
 }
 
 function clearBoard() {
--- sockets.js
+++ sockets.js
@@ -3,64 +3,12 @@ module.exports = function(io) {
   var monk = require('monk');
   var db = monk('localhost:27017/connect4');
   var board = db.get('gameboard');
+  var lastPlay = db.get('lastplay');
   var winchecker = require('./winchecker.js');
   var uaParser = require('ua-parser');
   io.sockets.on('connection', function (socket)
   {
     var UserAgent = uaParser.parse(socket.request.headers['user-agent']);
-    socket.on('play', function (data) {
-      console.log('play: ' + data.column);
-      // figure out which row this column gets.
-      if (data.column == undefined || data.column < 0 || data.column > 6) {
-        console.log('unable to drop.');
-        return false;
-      }
-      // equivalent to sql's select top order by. Get the highest column value.
-      board.find(
-        { 'column': data.column },
-        {'sort': {'row': -1}, limit: 1},
-        function (e, docs)
-        {
-          console.log(docs.length);
-          var insertRow = 0;
-          if (docs[0] != undefined)
-          {
-            insertRow = docs[0].row + 1
-          }
-          if (insertRow > 5)
-          {
-            return false;
-          }
-          console.log("Inserting at row " + insertRow, ", column " + data.column);
-          var SuccessfulPlay = {
-            'row': insertRow,
-            'column': data.column,
-            'agent': UserAgent.ua.family
-          };
-          board.insert(SuccessfulPlay, function (err, doc) {
-            if (err) {
-              console.log("there was a problem adding this crap.");
-            }
-            else {
-              console.log("Successfully inserted.")
-              board.find({},{}, function (e, docs) {
-
-                var winResult = winchecker.check(docs, SuccessfulPlay);
-                if (winResult.win) {
-                  //wait 5 seconds and clear the board.
-                  board.remove({}, function (err) {
-                    console.log(err);
-                  });
-                  io.emit('receiveWin', winResult);
-                }
-                console.log({"Title": "Win Result", "Value": winResult})
-              });
-            };
-          });
-          io.emit('receivePlay', SuccessfulPlay);
-      });
-    });
-
     socket.on('getBoard', function (data) {
       console.log('A user requested the board.')
       db.get('gameboard').find({}, {},
@@ -69,5 +17,100 @@ module.exports = function(io) {
           socket.emit("receiveBoard", docs);
         });
     });
+
+    socket.on('play', function (data) {
+      var canDrop = true;
+      console.log('Column: ' + data.column + ', Agent: ' + UserAgent.ua.family);
+      // figure out which row this column gets.
+      if (data.column == undefined
+        || data.column < 0
+        || data.column > 6) { // Out of range or undefined.
+        console.log('unable to drop.');
+        canDrop = false;
+      }
+      lastPlay.find({}, {}, function (e, docs) {
+        if (docs.length === 0) { // if last play is somehow empty.
+          io.emit('disableDrop')
+          lastPlay.insert({ agent: UserAgent.ua, time: new Date()},
+          function (err, doc) {
+            if (err) {
+              console.log(err);
+            }
+          });
+        }
+        else { //deny same agent two plays in a row.
+          if (docs[0].agent.family === UserAgent.ua.family) {
+            console.log(UserAgent.ua.family
+              + " played last. Let someone else have a go.");
+            canDrop = false;
+            io.emit('disableDrop');
+          }
+          else { // clear last player and insert current.
+            lastPlay.remove({}, function (err) {
+              console.log(err);
+            });
+            lastPlay.insert({ agent: UserAgent.ua, time: new Date()},
+            function (err, doc) {
+              if (err) {
+                console.log(err);
+              }
+            });
+          }
+        }
+      });
+      if (canDrop) {
+        // equivalent to sql's select top order by.
+        //Get the highest row value. for the column.
+        board.find(
+          { 'column': data.column },
+          {'sort': {'row': -1}, limit: 1},
+          function (e, docs)
+          {
+            console.log(docs.length);
+            var insertRow = 0;
+            if (docs[0] != undefined)
+            {
+              insertRow = docs[0].row + 1
+            }
+            if (insertRow > 5) { //The row is full! This is not allowed!
+              return false;
+            }
+            console.log("Inserting at row " + insertRow
+            + ", column " + data.column);
+            var SuccessfulPlay = {
+              'row': insertRow,
+              'column': data.column,
+              'agent': UserAgent.ua.family
+            };
+
+            board.insert(SuccessfulPlay, function (err, doc) {
+              if (err) {
+                debugger;
+                console.log(err);
+              }
+              else {
+                console.log("Successfully inserted.")
+                board.find({},{}, function (e, docs) {
+
+                  var winResult = winchecker.check(docs, SuccessfulPlay);
+                  if (winResult.win) {
+                    board.remove({}, function (err) { //clear the board.
+                      console.log(err);
+                    });
+                    lastPlay.remove({}, function (err) { // clear who played last.
+                      console.log(err);
+                      io.emit('enableDrop');
+                    });
+                    io.emit('receiveWin', winResult);
+                  }
+                  console.log({"Title": "Win Result", "Value": winResult})
+                });
+              };
+            });
+            io.emit('receivePlay', SuccessfulPlay);
+        });
+      }
+    });
+    // End of socket.on('play')
   });
 };
